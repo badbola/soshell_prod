865dadf67edb725e76bcd605eb6aa1ae5e35cc50
Badbola <divyanshuok@outlook.com>
2020-08-30 22:31:43 +0530

    Changes in socket

--- index.js
+++ index.js
@@ -33,7 +33,7 @@ if (env.name == "development") {
     })
   );
 }
-app.use(express.urlencoded());
+app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(env.asset_path));
 //make the uploads path available to browser
